07bf8fc6746b1c6036601923bbbe5fae778a89a8
alma <alma.gustafianto@mitrais.com>
2021-08-27 16:40:43 +0700

    fix

--- tms_api/config/dev.exs
+++ tms_api/config/dev.exs
@@ -3,7 +3,7 @@ use Mix.Config
 # Configure your database
 config :tms_api, TmsApi.Repo,
   username: "postgres",
-  password: "rahasia",
+  password: "postgres",
   database: "tms_api_dev",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
